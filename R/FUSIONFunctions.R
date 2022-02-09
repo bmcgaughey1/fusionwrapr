0db29378ff82893456750e2a6f689b859b09b824
Robert J. McGaughey <bmcgaughey1@gmail.com>
2022-02-08 16:38:57 -0800

    Added TreeSeg function and modified the use of quotes for some command line option in other program functions

--- R/FUSIONFunctions.R
+++ R/FUSIONFunctions.R
@@ -1086,8 +1086,8 @@ GridSurfaceCreate <- function(
   options <- addOption(options, class)
   options <- addOption(options, grid)
   options <- addOption(options, gridxy)
-  options <- addOption(options, align)
-  options <- addOption(options, extent)
+  options <- addOption(options, align, TRUE)
+  options <- addOption(options, extent, TRUE)
 
   # deal with required parameters...some may have defaults
   required <- addRequired(required, surfacefile, TRUE)
@@ -1346,8 +1346,8 @@ GridMetrics <- function(
   options <- addOption(options, rgb)
   options <- addOption(options, grid)
   options <- addOption(options, gridxy)
-  options <- addOption(options, align)
-  options <- addOption(options, extent)
+  options <- addOption(options, align, TRUE)
+  options <- addOption(options, extent, TRUE)
   options <- addOption(options, buffer)
   options <- addOption(options, cellbuffer)
   options <- addOption(options, strata)
@@ -1480,7 +1480,7 @@ GridMetrics <- function(
 #'   if \code{runCmd = FALSE}, return value is the (invisible) command line.
 #' @examples
 #' \dontrun{
-#' GridSurfaceCreate("test.dtm", 2.0, "M", "M", 1, 10, 2, 2, "Test/pts.las", class = "2")
+#' CanopyModel("test.dtm", 2.0, "M", "M", 1, 10, 2, 2, "Test/pts.las")
 #' }
 #' @export
 CanopyModel <- function(
@@ -1590,11 +1590,11 @@ CanopyModel <- function(
   options <- addOption(options, multiplier)
   options <- addOption(options, return)
   options <- addOption(options, class)
-  options <- addOption(options, ground)
+  options <- addOption(options, ground, TRUE)
   options <- addOption(options, hole)
   options <- addOption(options, grid)
   options <- addOption(options, gridxy)
-  options <- addOption(options, align)
+  options <- addOption(options, align, TRUE)
   options <- addOption(options, extent)
 
   # deal with required parameters...some may have defaults
@@ -1618,4 +1618,184 @@ CanopyModel <- function(
     invisible(buildCommand(cmd, options, required))
   }
 }
+
+# TreeSeg
+# ---------- TreeSeg
+#
+#' FUSION R command line interface -- Function to create command lines for the TreeSeg program.
+#'
+#' /code{CanopyModel} creates command lines for the FUSION TreeSeg program and optionally executes them.
+#'
+#' @param CHM character: Name for canopy height model (PLANS DTM with .dtm extension).
+#'   May be wildcard or text list file (extension .txt only). This can be a canopy surface
+#'   model if the /ground option is used to specify a ground surface for normalization.
+#' @param ht_threshold numeric: Minimum height for object segmentation. Portions of the CHM
+#'   below this height are not considered in the segmentation.
+#' @param outputfile character: Base name for output file. Metrics are stored in CSV format with
+#'   .csv extension. Other outputs are stored in files named using the base name and additional
+#'   descriptive information.
+#' @param quiet boolean: Suppress all output during the run.
+#' @param verbose boolean: Display all status information during the run.
+#' @param version boolean: Report version information and exit with no processing.
+#' @param newlog boolean: Erase the existing log file and start a new log
+#' @param log character: Use the name specified for the log file.
+#' @param locale boolean: Adjust program logic to input and output locale-specific numeric
+#'   formats (e.g. use a comma for the decimal separator).
+#' @param nolaszipdll boolean: Suppress the use of the LASzip dll (c) Martin Isenburg...
+#'   removes support for compressed LAS (LAZ) files. This option
+#'   is only useful for programs that read or write point files.
+#' @param height boolean: Normalize canopy surface model(s) using ground model(s).
+#' @param ptheight numeric: Normalize point heights using ground model(s).
+#' @param maxht numeric: Force the maximum height for the segmentation. This will override
+#'   the actual maximum value in the CHM. Use this option to force equal vertical resolution
+#'   across areas with varying maximum canopy heights.
+#' @param grid character: "X1,X2,Y1,Y2": Force the origin of the analysis grid to be (X,Y) instead of
+#'   computing an origin from the data extents and force the grid to be W units wide and H units
+#'   high...W and H will be rounded up to a multiple of cellsize.
+#' @param gridxy character: "X1,X2,Y1,Y2": Force the origin of the analysis grid to be (X1,Y1) instead
+#'   of computing an origin from the data extents and force the grid to use (X2,Y2) as the upper
+#'   right corner of the coverage area. The actual upper right corner will be adjusted to be a
+#'   multiple of cellsize.
+#' @param align character: Force the origin and extent of the analysis grid to match the
+#'   lower left corner and extent of the specified PLANS format DTM file.
+#' @param buffer numeric: Add a buffer to the data extent specified by /grid, /gridxy or /align
+#'   when segmenting but only output data for the segments located within the extent.
+#' @param ground character: Use the specified bare-earth surface model to normalize the LIDAR data
+#'   file may be wildcard or text list file (extension .txt only).
+#' @param points character: LIDAR point data file(s) in LDA or LAS format. May be wildcard or text
+#'   list file (extension .txt only). Points are assigned to individual basins or crown polygons
+#'   and a separate file (in LDA format) is output for each basin or polygon.
+#' @param segmentpts boolean: Output points for the raster segments. Default is to output points
+#'   for crown polygons when the /shape option is used and for raster segments when /shape is
+#'   not used. Used only with the /points option.
+#' @param aspect boolean: Calculate surface aspect.
+#' @param clipfolder character: folder name where point files for individual clips are stored. Used
+#'   only with the /points option. If not specified, point files are stored in the same folder with
+#'   other outputs. The folder name must end with a trailing backslash and must already exist.
+#' @param shape boolean: Create a shapefile containing the high points and basin metrics.
+#' @param cleantile boolean: Output an ASCII raster map that only includes basins within the
+#'   reporting extent defined by the /grid, /gridxy, and /align options.
+#' @param htmultiplier numeric: Multiply the high point and surface heights by this value for
+#'   output products. Also multiply individual point heights by # before writing point files
+#'   (see /points option).
+#' @param projection character: Associate the specified projection file with shapefile and raster
+#'   data products.
+#' @param use64bit boolean: indicates 64-bit version of the program
+#'   should be used.
+#' @param runCmd boolean: indicates command line should be executed.
+#' @param saveCmd boolean: indicates command line should be written to a file.
+#' @param cmdFile character: contains the name of the file to which commands
+#'   should be written.
+#' @param cmdClear boolean: indicates file for command should be deleted before the command
+#'   line is written.
+#' @param echoCmd boolean: indicates command line should be displayed.
+#' @return Return value depends on \code{runCmd}. if \code{runCmd = TRUE}, return value is
+#'   the (invisible) integer value return from the operating system after running the command.
+#'   if \code{runCmd = FALSE}, return value is the (invisible) command line.
+#' @examples
+#' \dontrun{
+#' TreeSeg("CHM.dtm", 2.0, "trees.csv")
+#' }
+#' @export
+TreeSeg <- function(
+  CHM = NULL,
+  ht_threshold = NULL,
+  outputfile = NULL,
+  quiet = FALSE,
+  verbose = FALSE,
+  version = FALSE,
+  newlog = FALSE,
+  log = NULL,
+  locale = FALSE,
+  nolaszipdll = FALSE,
+  height = FALSE,
+  ptheight = FALSE,
+  maxht = NULL,
+  grid = NULL,
+  gridxy = NULL,
+  align = NULL,
+  buffer = NULL,
+  ground = NULL,
+  points = NULL,
+  segmentpts = FALSE,
+  clipfolder = NULL,
+  shape = FALSE,
+  cleantile = FALSE,
+  htmultiplier = NULL,
+  projection = FALSE,
+  use64bit = TRUE,
+  runCmd = TRUE,
+  saveCmd = TRUE,
+  cmdFile = NULL,
+  cmdClear = FALSE,
+  echoCmd = FALSE
+) {
+  # check for required options
+  if (!isOpt(CHM)
+      || !isOpt(ht_threshold)
+      || !isOpt(outputfile)
+  ) {
+    stop("Missing required parameters: CHM, ht_threshold, outputfile")
+  }
+
+  # check for option to run command...if FALSE, check for command file name
+  checkRunSaveFile(runCmd, saveCmd, cmdFile)
+
+  # check for folder included in output...will create if it doesn't exist
+  verifyFolder(dirname(outputfile))
+
+  # check for folder for tree clips...will create if it doesn't exist
+  verifyFolder(dirname(clipfolder))
+
+  # build command line
+  cmd <- programName("TreeSeg", use64bit)
+
+  options <- ""
+  required <- ""
+
+  # deal with switches true/false...
+  # "standard" options
+  options <- addSwitch(options, quiet)
+  options <- addSwitch(options, verbose)
+  options <- addSwitch(options, version)
+  options <- addSwitch(options, newlog)
+  options <- addSwitch(options, locale)
+  options <- addSwitch(options, nolaszipdll)
+  options <- addOption(options, log, TRUE)
+
+  # program-specific options
+  options <- addSwitch(options, height)
+  options <- addSwitch(options, ptheight)
+  options <- addSwitch(options, segmentpts)
+  options <- addSwitch(options, shape)
+  options <- addSwitch(options, cleantile)
+
+  # deal with options...
+  options <- addOption(options, maxht)
+  options <- addOption(options, grid)
+  options <- addOption(options, gridxy)
+  options <- addOption(options, align, TRUE)
+  options <- addOption(options, buffer)
+  options <- addOption(options, ground, TRUE)
+  options <- addOption(options, points, TRUE)
+  options <- addOption(options, clipfolder, TRUE)
+  options <- addOption(options, htmultiplier)
+  options <- addOption(options, projection)
+
+  # deal with required parameters...some may have defaults
+  required <- addRequired(required, CHM, TRUE)
+  required <- addRequired(required, ht_threshold)
+  required <- addRequired(required, outputfile)
+
+  echoCommand(cmd, options, required, echoCmd)
+
+  ret <- dispatchCommand(cmd, options, required, runCmd, saveCmd, cmdClear, cmdFile)
+
+  if (runCmd) {
+    invisible(ret)
+  } else {
+    invisible(buildCommand(cmd, options, required))
+  }
+}
+
 
