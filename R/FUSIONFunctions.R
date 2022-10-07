1f75e7b93048710171df60e4df12bf1830acd176
bmcgaughey1 <bmcgaughey1@gmail.com>
2022-10-07 12:47:33 -0400

    Added CanopyMaxima and ReturnDensity functions

--- R/FUSIONFunctions.R
+++ R/FUSIONFunctions.R
@@ -1,4 +1,4 @@
-# FUSION function wrappers -- fusionR
+# FUSION function wrappers -- fusionwrapr
 #
 # found an rFUSION package https://cran.r-project.org/web/packages/rFUSION/index.html but not many functions
 # and it doesn't look very robust
@@ -8,7 +8,7 @@
 # programs...do the math!
 #
 # It might be really helpful if I could write functions to read/write DTM format files
-#
+# Done!
 #
 # Some useful keyboard shortcuts for package authoring:
 #
@@ -2836,4 +2836,338 @@ SurfaceSample <- function(
   }
 }
 
+# CanopyMaxima
+# ---------- CanopyMaxima
+#
+#' FUSION R command line interface -- Finds and reports surface maxima using a variable-size window based on surface height.
+#'
+#' \code{CanopyMaxima} creates command lines for the FUSION CanopyMaxima program and optionally executes them.
+#'
+#' The default equation used to compute the window size was developed using metric LIDAR return data (XYZ in meters).
+#' When using other measurement units, different equation coefficients are needed.
+#'
+#' The default behavior is to append new trees to an existing file. If you mix outputs from runs with different
+#' command line options, column labels may not make sense. This is especially true with the /crad and /minmax options.
+#'
+#' @template MultipleCommands
+#'
+#' @param inputfile character (\strong{required}):  Name of the canopy height model file (PLANS DTM with .dtm extension).
+#' @param outputfile character (\strong{required}): Name for the output ASCII CSV file that will contain the maxima information.
+#' @template StandardOptions
+#'
+#' @param ground character: Use the specified bare-earth surface model to normalize the LIDAR data
+#'   file may be wildcard or text list file (extension .txt only).
+#' @param threshold numeric: Limit analysis to areas above a height of # units (default: 10.0).
+#' @param wse character: "A,B,C,D[,E,F]": Constant and coefficients for the variable window size
+#'  equation used to compute the window size given the canopy surface height window:
+#'  width = A + B*ht + C*ht^2 + D*ht^3
+#'  (defaults for metric: A = 2.51503, B = 0, C = 0.00901, D = 0)
+#'  Use A = 8.251, B = 0, C = 0.00274, D = 0 for imperial units .
+#'  E and F parameters are for 4th and 5th order polynomial terms.
+#' @param mult numeric: Window size multiplier (default: 1.0). Applied after the window size is computed using
+#'  \code{wse} options.
+#' @param res numeric: Resolution multiplier for intermediate grids (default: 2.0). A value of 2 results in intermediate
+#'  grids with twice the number of rows and columns.
+#' @param outxy character: "minx,miny,maxx,maxy": Restrict output of tree located outside of the extent defined by
+#'  (minx,miny) and (maxx,maxy). Tree on the left and bottom edges will be output, those on the top and right edges will not.
+#' @param crad boolean: Output 16 individual crown radii for each tree. Radii start at 3 o'clock and are in counter-clockwise
+#'  order at 22.5 degree intervals.
+#' @param shape boolean: Create shapefile outputs for the canopy maxima points and the perimeter of the area associated with each maxima.
+#' @param img8 boolean: Create an 8-bit image showing local maxima and minima (use when 24 bit image fails due to large canopy model).
+#' @param img24 boolean: Create an 24-bit image showing local maxima and minima.
+#' @param new boolean: Create a new output file (erase output file if one exists).
+#' @param summary boolean: Produce a summary file containing tree height summary statistics.
+#' @param projection character: "filename": Associate the specified projection file with shapefile and raster data products.
+#' @param minmax numeric: Change the calculation method for the min/max crown width.
+#'  Options: 0 = report the max and min crown radii * 2
+#'  1 = report the max and min diameters
+#'  2 = report N-S diameter and the E-W diameter
+#'  3 = report max diameter and the diameter perpendicular to the max diameter line and the rotation to the max line.
+#' @template Use64bit
+#' @template RunSaveOptions
+#' @template Comment
+#' @return Return value depends on \code{runCmd}. if \code{runCmd = TRUE}, return value is
+#'   the (invisible) integer value return from the operating system after running the command.
+#'   if \code{runCmd = FALSE}, return value is the (invisible) command line.
+#' @examples
+#' \dontrun{
+#' CanopyMaxima("chm.dtm", "out.csv")
+#' }
+#' @family LTKFunctions
+#' @export
+CanopyMaxima <- function(
+    inputfile = NULL,
+    outputfile = NULL,
+    quiet = FALSE,
+    verbose = FALSE,
+    version = FALSE,
+    newlog = FALSE,
+    log = NULL,
+    locale = FALSE,
+    ground = NULL,
+    threshold = NULL,
+    wse = NULL,
+    mult = NULL,
+    res = NULL,
+    outxy = NULL,
+    crad = FALSE,
+    shape = FALSE,
+    img8 = FALSE,
+    img24 = FALSE,
+    new = FALSE,
+    summary = FALSE,
+    projection = NULL,
+    minmax = NULL,
+    runCmd = TRUE,
+    saveCmd = TRUE,
+    cmdFile = NULL,
+    cmdClear = FALSE,
+    echoCmd = FALSE,
+    comment = NULL
+) {
+  # check for required options
+  if (!isOpt(inputfile)
+      || !isOpt(outputfile)
+  ) {
+    stop("Missing required parameters: inputfile, outputfile")
+  }
+
+  # use the global variables to set command dispatch options...global options
+  # are only used if the corresponding option was not passed to the function
+  if (fusionrEnv$areSet) {
+    if (missing(runCmd)) runCmd <- fusionrEnv$runCmd
+    if (missing(saveCmd)) saveCmd <- fusionrEnv$saveCmd
+    if (missing(cmdFile)) cmdFile <- fusionrEnv$cmdFile
+    if (missing(echoCmd)) echoCmd <- fusionrEnv$echoCmd
+  }
+
+  # check for option to run command...if FALSE, check for command file name
+  checkRunSaveFile(runCmd, saveCmd, cmdFile)
+
+  # check for folder included in output...will create if it doesn't exist
+  verifyFolder(dirname(outputfile), runCmd, saveCmd, cmdFile, cmdClear)
+
+  # if we are saving commands to a file, cmdClear will have done its job in the call to verifyFolder
+  cmdClear <- FALSE
+
+  # build command line
+  cmd <- programName("CanopyMaxima", FALSE)
+
+  options <- ""
+  required <- ""
+
+  # deal with switches true/false...
+  # "standard" options
+  options <- addSwitch(options, quiet)
+  options <- addSwitch(options, verbose)
+  options <- addSwitch(options, version)
+  options <- addSwitch(options, newlog)
+  options <- addSwitch(options, locale)
+  options <- addOption(options, log, TRUE)
+
+  # program-specific options
+  options <- addSwitch(options, crad)
+  options <- addSwitch(options, shape)
+  options <- addSwitch(options, img8)
+  options <- addSwitch(options, img24)
+  options <- addSwitch(options, new)
+  options <- addSwitch(options, summary)
+
+  # deal with options...
+  options <- addOption(options, ground)
+  options <- addOption(options, threshold)
+  options <- addOption(options, wse)
+  options <- addOption(options, mult)
+  options <- addOption(options, res)
+  options <- addOption(options, outxy)
+  options <- addOption(options, projection)
+  options <- addOption(options, minmax)
+
+  # deal with required parameters...some may have defaults
+  required <- addRequired(required, inputfile, TRUE)
+  required <- addRequired(required, outputfile, TRUE)
+
+  echoCommand(cmd, options, required, echoCmd)
+
+  ret <- dispatchCommand(cmd, options, required, runCmd, saveCmd, cmdClear, cmdFile, comment)
+
+  if (runCmd) {
+    invisible(ret)
+  } else {
+    invisible(buildCommand(cmd, options, required))
+  }
+}
+
+# ReturnDensity
+# ---------- ReturnDensity
+#
+#' FUSION R command line interface -- Builds a raster data layer containing the number of returns in each cell.
+#'
+#' \code{ReturnDensity} creates command lines for the FUSION ReturnDensity program and optionally executes them.
+#'
+#' @template MultipleCommands
+#'
+#' @param outputfile character (\strong{required}): Name for the output raster file (stored in PLANS DTM format
+#'  with .dtm extension). When \code{ascii = TRUE}, the output is stored in ASCII raster format
+#' @param cellsize numeric (\strong{required}): Desired grid cell size in the same units as LIDAR data.
+#' @param datafile character (\strong{required}): Name(s) of lidar data files.
+#' @template StandardOptions
+#' @template SkipFileCheck
+#' @param first boolean: Use first returns when computing return counts (default is all returns).
+#'  Cannot be combined with /last or /only.
+#' @param last boolean: Use last returns when computing return counts (default is all returns). Only valid for
+#'  LAS or LAZ format files. Cannot be combined with /first or /only.
+#' @param only boolean: Use last returns from pulses with only 1 return when computing return counts (default
+#'  is allreturns). Only valid for LAS or LAZ format files. Cannot be combined with /first or /last.
+#' @param ascii boolean: Output raster data in ASCII raster format instead of PLANS DTM format. Output file extension will be .asc.
+#' @param nointpercentile boolean: Do not compute percentile values associated with first return intensity and
+#'  output to CSV file. These values are useful to determine the range of values to scale intensity products.
+#' @param projection character: "filename": Associate the specified projection file with ASCII raster products.
+#' @param class character: "#,#,#,...": LAS files only: Specifies that only points with classification
+#'   values listed are to be included in the subsample.
+#'   Classification values should be separated by a comma.
+#'   e.g. (2,3,4,5) and can range from 0 to 31.
+#'   If the first character in string is ~, the list is interpreted
+#'   as the classes you DO NOT want included in the subsample.
+#'   e.g. /class:~2,3 would include all class values EXCEPT 2 and 3.
+#' @param ignoreoverlap boolean: Ignore points with the overlap flag set (LAS V1.4+ format).
+#' @param grid character: "X1,X2,Y1,Y2": Force the origin of the output grid to be (X,Y) instead of
+#'   computing an origin from the data extents and force the grid to
+#'   be W units wide and H units high...W and H will be rounded up to
+#'   a multiple of cellsize.
+#' @param gridxy character: "X1,X2,Y1,Y2": Force the origin of the output grid to be (X1,Y1) instead
+#'   of computing an origin from the data extents and force the grid
+#'   to use (X2,Y2) as the upper right corner of the coverage area.
+#'   The actual upper right corner will be adjusted to be a multiple
+#'   of cellsize.
+#' @param align character: Force the origin and extent of the output grid to match the
+#'   lower left corner and extent of the specified PLANS format DTM file.
+#' @param extent character: Force the origin and extent of the output grid to match the
+#'   lower left corner and extent of the specified PLANS format DTM
+#'   file but adjust the origin to be an even multiple of the cell
+#'   size and the width and height to be multiples of the cell size.
+#' @param datacoverage boolean: Check for points within each cell in the output grid. Output a value of 1 if
+#'  there are points covering the cell and a value of 0 if not.
+#' @template Use64bit
+#' @template RunSaveOptions
+#' @template Comment
+#' @return Return value depends on \code{runCmd}. if \code{runCmd = TRUE}, return value is
+#'   the (invisible) integer value return from the operating system after running the command.
+#'   if \code{runCmd = FALSE}, return value is the (invisible) command line.
+#' @examples
+#' \dontrun{
+#' GridMetrics("points/*.las", "test.csv", minht = 2.0)
+#' }
+#' @family LTKFunctions
+#' @export
+ReturnDensity <- function(
+    outputfile = NULL,
+    cellsize = NULL,
+    datafile = NULL,
+    quiet = FALSE,
+    verbose = FALSE,
+    version = FALSE,
+    newlog = FALSE,
+    log = NULL,
+    locale = FALSE,
+    nolaszipdll = FALSE,
+    skipfilecheck = FALSE,
+    first = FALSE,
+    last = FALSE,
+    only = FALSE,
+    ascii = FALSE,
+    nointpercentile = FALSE,
+    projection = NULL,
+    class = NULL,
+    ignoreoverlap = FALSE,
+    grid = NULL,
+    gridxy = NULL,
+    align = NULL,
+    extent = NULL,
+    datacoverage = FALSE,
+    use64bit = TRUE,
+    runCmd = TRUE,
+    saveCmd = TRUE,
+    cmdFile = NULL,
+    cmdClear = FALSE,
+    echoCmd = FALSE,
+    comment = NULL
+) {
+  # check for required options
+  if (!isOpt(outputfile)
+      || !isOpt(cellsize)
+      || !isOpt(datafile)
+  ) {
+    stop("Missing required parameters: outputfile, cellsize, datafile")
+  }
+
+  # use the global variables to set command dispatch options...global options
+  # are only used if the corresponding option was not passed to the function
+  if (fusionrEnv$areSet) {
+    if (missing(use64bit)) use64bit <- fusionrEnv$use64bit
+    if (missing(runCmd)) runCmd <- fusionrEnv$runCmd
+    if (missing(saveCmd)) saveCmd <- fusionrEnv$saveCmd
+    if (missing(cmdFile)) cmdFile <- fusionrEnv$cmdFile
+    if (missing(echoCmd)) echoCmd <- fusionrEnv$echoCmd
+  }
+
+  # check for option to run command...if FALSE, check for command file name
+  checkRunSaveFile(runCmd, saveCmd, cmdFile)
+
+  # check for folder included in output...will create if it doesn't exist
+  verifyFolder(dirname(outputfile), runCmd, saveCmd, cmdFile, cmdClear)
+
+  # if we are saving commands to a file, cmdClear will have done its job in the call to verifyFolder
+  cmdClear <- FALSE
+
+  # build command line
+  cmd <- programName("ReturnDensity", use64bit)
+
+  options <- ""
+  required <- ""
+
+  # deal with switches true/false...
+  # "standard" options
+  options <- addSwitch(options, quiet)
+  options <- addSwitch(options, verbose)
+  options <- addSwitch(options, version)
+  options <- addSwitch(options, newlog)
+  options <- addSwitch(options, locale)
+  options <- addSwitch(options, nolaszipdll)
+  options <- addOption(options, log, TRUE)
+  options <- addSwitch(options, skipfilecheck)
+
+  # program-specific options
+  options <- addSwitch(options, first)
+  options <- addSwitch(options, last)
+  options <- addSwitch(options, only)
+  options <- addSwitch(options, ascii)
+  options <- addSwitch(options, nointpercentile)
+  options <- addSwitch(options, ignoreoverlap)
+  options <- addSwitch(options, datacoverage)
+
+  # deal with options...
+  # program-specific options
+  options <- addOption(options, projection)
+  options <- addOption(options, class)
+  options <- addOption(options, grid)
+  options <- addOption(options, gridxy)
+  options <- addOption(options, align, TRUE)
+  options <- addOption(options, extent, TRUE)
+
+  # deal with required parameters...some may have defaults
+  required <- addRequired(required, outputfile, TRUE)
+  required <- addRequired(required, cellsize, TRUE)
+  required <- addRequired(required, datafile, TRUE)
+
+  echoCommand(cmd, options, required, echoCmd)
+
+  ret <- dispatchCommand(cmd, options, required, runCmd, saveCmd, cmdClear, cmdFile, comment)
+
+  if (runCmd) {
+    invisible(ret)
+  } else {
+    invisible(buildCommand(cmd, options, required))
+  }
+}
 
---
+++ man-roxygen/SkipFileCheck.R
@@ -0,0 +1,3 @@
+#' @param skipfilecheck boolean: Skip logic that checks for valid point files and removes those that are invalid
+#'  prior to the start of point processing. This option is designed to overcome some limitations with windows
+#'  and wildcard processing. This option is only useful for programs that read point data.
